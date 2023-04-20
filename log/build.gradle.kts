44ceff15914c47c86643576bef2302ae0405950b
Ming Li <fox20431@gmail.com>
2023-04-20 15:37:13 +0800

    add a log module

---
+++ log/build.gradle.kts
@@ -0,0 +1,11 @@
+group = "com.hihusky.log"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    // logback
+    implementation("ch.qos.logback:logback-classic:1.2.6")
+    // logback encoder
+    implementation("net.logstash.logback:logstash-logback-encoder:6.6")
+    // slf4j
+    implementation("org.slf4j:slf4j-api:1.7.32")
+}
